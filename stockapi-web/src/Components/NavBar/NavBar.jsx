bb2ef533d86538d1ffe686f3a75b9215cbdb59f3
dbrosio2 <dbrosio2@belatrixsf.com>
2019-10-08 10:15:07 -0300

    Added navbar icons

--- stockapi-web/src/Components/NavBar/NavBar.jsx
+++ stockapi-web/src/Components/NavBar/NavBar.jsx
@@ -5,6 +5,9 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from 'mdi-react/MenuIcon';
+import SearchIcon from '@material-ui/icons/Search';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import PersonIcon from '@material-ui/icons/Person';
 
 import './navBar.css';
 
@@ -21,6 +24,11 @@ const styles = {
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-between',
+    },
+    iconButton:{
+      edge: 'start', 
+      color: 'inherit',
+      fontSize: 'inherit' ,
     }
 }
 
@@ -30,12 +38,22 @@ const NavBar = () => {
       <AppBar position="static" color="inherit" style={styles.appBar}>
         <Toolbar style={styles.toolBar}>
           <IconButton edge="start"  color="inherit" aria-label="menu">
-            <MenuIcon />
+            <MenuIcon fontSize="inherit" />
           </IconButton>
           <Typography variant="h8" >
             Stock App
           </Typography>
-          <Button color="inherit">Login</Button>
+          <div className="iconContainer">
+            <IconButton style={styles.iconButton} aria-label="search">
+              <SearchIcon fontSize="inherit" />
+            </IconButton>
+            <IconButton style={styles.iconButton}  aria-label="search">
+              <HelpOutlineIcon fontSize="inherit"  />
+            </IconButton>
+            <IconButton style={styles.iconButton}  aria-label="search">
+              <PersonIcon fontSize="inherit" />
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
     </div>
