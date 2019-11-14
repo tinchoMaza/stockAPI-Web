d7a917ed7afbb612e244faeb1dcb99db97ee5b64
dbrosio2 <dbrosio2@belatrixsf.com>
2019-11-14 10:43:35 -0300

    refactored sidebar

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,49 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import SideBarLinksContainer from './SideBarLinksContainer'
+import { makeStyles } from '@material-ui/core/styles'
 
-const style = (width) => { 
-    return {
+const useStyles = makeStyles(theme => ({
+    sideBar : props => ({
         transition: '0.25s width',
         position: 'fixed',
-        width: width,
+        width: props.visible? 200: 0,
         backgroundColor: '#D9D9D9',
         height: 'calc(100vh - 64px)',
-    }
+    })
+}))
+
+export default function SideBar(props) {
+    const classes = useStyles(props)
+
+    return (
+        <div className={classes.sideBar} >
+            { props.visible && <SideBarLinksContainer/> }
+        </div>
+    )
 }
-
-
-export default class SideBar extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { 
-            visible: true,
-            style: style('200px')
-        }
-    }
-    
-    componentWillReceiveProps(nextProps) {
-        if(nextProps.visible){
-            this.setState({ 
-                visible: nextProps.visible,
-                style: style('200px')
-            });  
-        }
-        else {
-            this.setState({ 
-                visible: nextProps.visible,
-                style: style('0')
-            });  
-        }
-
-    }
-    
-    render() {
-        return (
-            <div className="side-bar" style={this.state.style}>
-                { this.state.visible && <SideBarLinksContainer unaprop={"queseuo"}/> }
-            </div>
-        )
-    }
-}
-
-// export default withRouter(props => <SideBar {...props}/>);
+       
