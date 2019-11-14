c64fe3de51b3401a10122e0d2b5d5749921bb904
dbrosio2 <dbrosio2@belatrixsf.com>
2019-11-14 10:52:23 -0300

    refactored subcategories

--- src/Components/SideBar/SubItems/OrdersSubItems.jsx
+++ src/Components/SideBar/SubItems/OrdersSubItems.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
 
 export default function OrdersSubItems() {
+    const classes = useStyles();
+
     return (
-        <div>
-            <p>Orders option 1</p>
-            <p>Orders option 2</p>
+        <div className={classes.categories}>
+            <Typography>Category 1</Typography>                                
+            <Typography>Category 2</Typography>                                
+            <Typography>Category 3</Typography>                                
         </div>
     )
 }
+
+const useStyles = makeStyles(theme => ({
+    categories: {
+        color: theme.palette.grey[600],
+        marginLeft: theme.spacing(3),
+        color: theme.palette.grey[700],
+        '& p':{fontSize: '.95em'},
+        '& a': {
+            color: 'inherit',
+            textDecoration: 'none',
+        }
+    },
+}))
--- src/Components/SideBar/SubItems/SettingsSubItems.jsx
+++ src/Components/SideBar/SubItems/SettingsSubItems.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
 
 export default function SettingsSubItems() {
+    const classes = useStyles();
+
     return (
-        <div>
-            <p>Settings category 1</p>
-            <p>Settings category 2</p>
-            <p>Settings category 3</p>
+        <div className={classes.categories}>
+            <Typography>Category 1</Typography>                                
+            <Typography>Category 2</Typography>                                
+            <Typography>Category 3</Typography>                                
         </div>
     )
 }
+
+const useStyles = makeStyles(theme => ({
+    categories: {
+        color: theme.palette.grey[600],
+        marginLeft: theme.spacing(3),
+        color: theme.palette.grey[700],
+        '& p':{fontSize: '.95em'},
+        '& a': {
+            color: 'inherit',
+            textDecoration: 'none',
+        }
+    },
+}))
