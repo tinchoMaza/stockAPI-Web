e80e0361206dd34b29e2e1aba550bb86eaa8253a
dbrosio2 <dbrosio2@belatrixsf.com>
2019-12-05 11:20:57 -0300

    added theming

--- src/Components/MainContainer/Tests/DrawImage.jsx
+++ src/Components/MainContainer/Tests/DrawImage.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import JSONPretty from 'react-json-prettify';
 import { withStyles } from '@material-ui/styles';
-
-const colors = ["lightGreen","red","lightBlue","orange","blue"]
+import { Grid } from '@material-ui/core';
+import {github} from 'react-json-prettify/dist/themes';
 
 class DrawImage extends Component{
 
@@ -17,43 +17,89 @@ class DrawImage extends Component{
             var facesCounter = 0;
             var top, left, height, width, rotation;
             ctx.drawImage(img, 0, 0)
-            ctx.font = "18px Courier";
+            ctx.font = "24px Courier";
             faces.forEach((face)=>{
                 top = face.faceRectangle.top;
                 left = face.faceRectangle.left;
                 height = face.faceRectangle.height;
                 width = face.faceRectangle.width;
                 rotation = face.faceAttributes.headPose.roll;
-                ctx.fillStyle = colors[facesCounter];
                 
                 ctx.save();
-                ctx.fillText(face.faceId.substring(24,36), left, top+height+18 )
+                ctx.fillStyle = colors.vivid[facesCounter%8];
+                ctx.strokeStyle = colors.vivid[facesCounter%8];
                 ctx.beginPath();
                 ctx.lineWidth = "2";
-                ctx.strokeStyle = colors[facesCounter];
                 ctx.translate( left+width/2, top+height/2 );
                 ctx.rotate(rotation * Math.PI / 180);
                 ctx.rect( -width/2, -height/2, width,height);
                 ctx.stroke();
+                ctx.fillText(facesCounter+1, +width/2, -height/2 )
                 facesCounter++;
                 ctx.restore();
             })
         }
     }
 
+    
     render() {
         const { classes } = this.props;
+        var facesCounter = -1;
+        var faceInfo;
         return(
             <div className={classes.container}>
                 <div>
                     <img className={classes.image} ref="image" src={this.props.url}></img>
                     <canvas className={classes.canva} ref="canvas" width={this.props.size.width} height={this.props.size.height} />
                 </div>
-                <JSONPretty json={this.props.res}/>
+                <Grid container className={classes.grid} spacing={2}>
+                    {this.props.res.map( (face) => {
+                        facesCounter++;
+                        faceInfo = {
+                            faceCounter : facesCounter,
+                            ...face
+                        }
+                        return(
+                            <Grid item >
+                                <JSONPretty json={faceInfo} theme={customTheme(facesCounter%8)}/>
+                            </Grid>
+                        )
+                    })}
+                </Grid>
+
+
             </div>
         )
     }
 }
+
+const colors = {
+    vivid:[
+        "rgb(255,152,0)",
+        "rgb(233,30,99)",
+        "rgb(103,58,183)",
+        "rgb(33,150,243)",
+        "rgb(139,195,74)",
+        "rgb(244,67,54)",
+        "rgb(0,150,136)",
+        "rgb(205,220,57)",
+    ],
+    transparent:[
+        "rgb(255,152,0,0.25)",
+        "rgb(233,30,99,0.25)",
+        "rgb(103,58,183,0.25)",
+        "rgb(33,150,243,0.25)",
+        "rgb(139,195,74,0.25)",
+        "rgb(244,67,54,0.25)",
+        "rgb(0,150,136,0.25)",
+        "rgb(205,220,57,0.25)",
+    ]
+}
+
+const customTheme = (counter) => ({
+    ...github,
+    background: colors.transparent[counter],
+});
 
 const styles = theme => ({
     container:{
@@ -61,6 +107,11 @@ const styles = theme => ({
         flexDirection: 'column',
         justifyContent: 'flex-start',
         alignItems: 'center',
+    },
+    grid: {
+        margin: 'auto',
+        justifyContent: 'center',
+
     },
     image:{
         display: 'none'
